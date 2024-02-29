ad4f8baf0c8cde07c4ff4ef1742f6b5b2d498903
saeid9419 <68286305+saeid9419@users.noreply.github.com>
2024-02-29 13:34:29 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function sum(){
+    
+    
+    let num1=Number(document.getElementById('num1').value)
+    let num2=Number(document.getElementById('num2').value)
+    
+    let result=num1+num2
+    
+    document.getElementById('result').value=result
+
+}
